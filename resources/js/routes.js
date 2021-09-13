8a64fc04a4d2a236fbe0ce1caa4eb7e506a4f374
Eva Maria <evamariacamacho@gmail.com>
2021-09-13 15:48:43 +0200

    first commit

---
+++ resources/js/routes.js
@@ -0,0 +1,36 @@
+const Home = () => import('./compoments/Home.vue')
+const Contacto = () => import('./compoments/Contacto.vue')
+
+//importamos los componentes para el blog
+
+const Mostrar = () => import('./compoments/blog/Mostrar.vue')
+const Crear = () => import('./compoments/blog/Crear.vue')
+const Editar = () => import('./compoments/blog/Editar.vue')
+
+export const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: Home
+    },
+    {
+        name: 'mostrarBlogs',
+        path: '/blogs',
+        component: Mostrar
+    },
+    {
+        name: 'crearBlog',
+        path: '/crear',
+        component: Crear
+    },
+    {
+        name: 'editarBlog',
+        path: '/editar/:id',
+        component: Editar
+    },
+    {
+        name: 'contacto',
+        path: '/contacto',
+        component: Contacto
+    }
+]
